f54d94bed9dbe888e22b4383a608bdb6c42e483b
raxokoron <raxokoron@gmail.com>
2013-09-29 07:24:11 +0100

    STL output logging macro change;

--- DLogCore/STLOutput.cpp
+++ DLogCore/STLOutput.cpp
@@ -1,19 +1,29 @@
 #include "../DLog.h"
 #include "DLogInitializer.h"
 
+#ifndef NO_STL
+
+#ifdef STL_OUTPUT
 #include <iostream>
 
 void STLOutputFunction(Severity severity, const char* functionName, unsigned int line, const char* msg)
 {
 	static const char* severity_msg[] = {
-		"  INFO   ", "__WARNING__ ", "~~ERROR~~ ", "##FATAL## ", "!!ASSERT!! "
+		"   INFO     ", 
+		"__WARNING__ ", 
+		" ~~ERROR~~  ", 
+		" ##FATAL##  ", 
+		" !!ASSERT!! "
 	};
 	std::cout << severity_msg[static_cast<int>(severity)] << functionName << '(' << line << "): " << msg << std::endl;
 }
 
 static DLogInitializer init(STLOutputFunction);
+#endif // STL_OUTPUT
 
 DLogger& operator<<(DLogger& d, const std::string& item)
 {
 	return d.print(item.c_str());
 }
+
+#endif // NO_STL
--- DLogCore/STLOutput.h
+++ DLogCore/STLOutput.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#ifndef NO_STL
+
 #include <string>
 
 class DLogger;
-
 DLogger& operator<<(DLogger& d, const std::string& item);
 
+#ifdef STL_OUTPUT
+// USE THIS MACRO TO ENABLE STL OUTPUT
+#endif
 
+#endif // NO_STL
