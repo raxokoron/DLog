aedca6661c1154ff3a6d24a479939cd899d25ed7
raxokoron <raxokoron@gmail.com>
2013-09-29 06:51:03 +0100

    STLOutput function updated;

--- DLogCore/STLOutput.cpp
+++ DLogCore/STLOutput.cpp
@@ -6,9 +6,9 @@
 void STLOutputFunction(Severity severity, const char* functionName, unsigned int line, const char* msg)
 {
 	static const char* severity_msg[] = {
-		" ", "__WARNING__ ", "~~ERROR~~ ", "##FATAL## ", "!!ASSERT!! "
+		"  INFO   ", "__WARNING__ ", "~~ERROR~~ ", "##FATAL## ", "!!ASSERT!! "
 	};
-	std::cout << severity_msg[static_cast<int>(severity)] << functionName << '(' << line << ')' << msg;
+	std::cout << severity_msg[static_cast<int>(severity)] << functionName << '(' << line << "): " << msg << std::endl;
 }
 
 static DLogInitializer init(STLOutputFunction);
